a33da3f68eb81386f612482022c91a66a27b58d9
counter185 <33550839+counter185@users.noreply.github.com>
2023-02-21 19:23:51 +0100

    Initial commit 2

---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BR.UI;
+using System;
+using UnityEngine;
+
+namespace BlueRefSun_GraphicsPatch
+{
+    [BepInPlugin("pl.cntrpl.bluerefsungraphicspatch", "BRS Graphics Patch", "1.0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin pl.cntrpl.bluerefsungraphicspatch is loaded!");
+            Parameter.TargetFrameRateType = Parameter.FrameRateType.FPS60;
+            Screen.SetResolution(1920, 1080, true);
+        }
+
+        private void Update()
+        {
+            //needs to be done in update because the game likes to change this value at random times
+            Parameter.TargetFrameRateType = Parameter.FrameRateType.FPS60;
+            if (Input.GetKeyDown(KeyCode.F12))
+            {
+                Screen.SetResolution(1920, 1080, true);
+            }
+        }
+    }
+}
